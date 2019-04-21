312cef8aa13bd486a5aeb51775ac7596e48d9c06
Daniel Hamilton <d.hamilton@ufl.edu>
2019-04-20 22:00:55 -0400

    menu to navigate 4 games with unique threads

--- inc/menu.h
+++ inc/menu.h
@@ -11,8 +11,73 @@
 #include "time.h"
 #include "math.h"
 #include "common_threads.h"
-
+#include "game1.h"
+#include "game2.h"
+#include "game3.h"
+#include "game4.h"
 /*********************************************** Includes ********************************************************************/
+
+#define RED_ON              P2->OUT |= BIT0
+#define RED_OFF             P2->OUT &= ~BIT0
+#define GREEN_ON            P2->OUT |= BIT1
+#define GREEN_OFF           P2->OUT &= ~BIT1
+#define BLUE_ON             P2->OUT |= BIT2
+#define BLUE_OFF            P2->OUT &= ~BIT2
+
+/*
+ * ======================
+ * =    Q1   |    Q2    =
+ * ======================
+ * =    Q3   |    Q4    =
+ * ======================
+ */
+#define QUAD_1_MAX_X        MAX_SCREEN_X / 2
+#define QUAD_1_MIN_X        0
+#define QUAD_1_MAX_Y        MAX_SCREEN_Y / 2
+#define QUAD_1_MIN_Y        0
+
+#define QUAD_2_MAX_X        MAX_SCREEN_X
+#define QUAD_2_MIN_X        MAX_SCREEN_X / 2
+#define QUAD_2_MAX_Y        MAX_SCREEN_Y / 2
+#define QUAD_2_MIN_Y        0
+
+#define QUAD_3_MAX_X        MAX_SCREEN_X / 2
+#define QUAD_3_MIN_X        0
+#define QUAD_3_MAX_Y        MAX_SCREEN_Y
+#define QUAD_3_MIN_Y        MAX_SCREEN_Y / 2
+
+#define QUAD_4_MAX_X        MAX_SCREEN_X
+#define QUAD_4_MIN_X        MAX_SCREEN_X / 2
+#define QUAD_4_MAX_Y        MAX_SCREEN_Y
+#define QUAD_4_MIN_Y        MAX_SCREEN_Y / 2
+
+/*********************************************** Typedefs ********************************************************************/
+
+// CLIENT TO HOST data structure
+typedef struct
+{
+    uint32_t IP_address;    // return data information
+    uint8_t playerNumber;   // return data information
+    bool joined;            // handshaking data
+    bool acknowledge;       // handshaking data
+} ClientToHost_t;
+
+/*
+ * HOST TO CLIENT data structure
+ * Game Number : 0 => top left
+ *               1 => top right
+ *               2 => bottom left
+ *               3 => bottom right
+*/
+typedef struct
+{
+    ClientToHost_t client;
+    int8_t game_number;
+    int8_t prev_game_number;
+    bool choice_made;
+} HostToClient_t;
+/*********************************************** Typedefs ********************************************************************/
+
 
 /*********************************************** Semaphores ********************************************************************/
 extern semaphore_t CC3100_SEMAPHORE;
@@ -20,15 +85,23 @@ extern semaphore_t LCDREADY;
 /*********************************************** Semaphores ********************************************************************/
 
 /*********************************************** Functions ********************************************************************/
-void menu_writeMainMenu();
-void menu_eraseMainMenu();
+void menu_writeHostOrClient();
+void menu_eraseHostOrClient();
+void menu_addThreadsHost ( void );
+void menu_addThreadsClient ( void );
+void menu_initMenu ( void );
 /*********************************************** Functions ********************************************************************/
 
 /*********************************************** Host Threads *********************************************************************/
 void menu_MenuHost ( void );
+void menu_SendDataToClient ( void );
+void menu_JoystickHost ( void );
+void ExitMenuHost ( void );
 
 /*********************************************** Host Threads *********************************************************************/
 void menu_MenuClient ( void );
+void menu_ReceiveDataFromHost ( void );
+void ExitMenuClient ( void );
 
 /*********************************************** Aperiodic Threads *********************************************************************/
 void menu_ButtonPress ( void );
--- src/main.c
+++ src/main.c
@@ -24,11 +24,11 @@
 #include "cc3100_usage.h"
 #include "LCD_empty.h"
 #include "common_threads.h"
-#include "menu.h"
 #include "game1.h"
 #include "game2.h"
 #include "game3.h"
 #include "game4.h"
+#include "menu.h"
 
 // MACROS --------------------------------------------
 #define TP_ENABLE   1
@@ -47,15 +47,12 @@ void main(void)
     G8RTOS_AddAperiodicEvent_Priority(&common_ButtonPress, 0, PORT4_IRQn);
     G8RTOS_AddAperiodicEvent_Priority(&common_ButtonPress, 0, PORT5_IRQn);
 
-    // write the menu text
-    menu_writeMainMenu();
-
     // Initialize semaphores
     G8RTOS_InitSemaphore(&CC3100_SEMAPHORE, 1);
 	G8RTOS_InitSemaphore(&LCDREADY, 1);
   
     // write the menu text
-    menu_writeMainMenu();
+    menu_writeHostOrClient();
 
     // Do not initialize any of the threads until the user decides
     // whether to host the game or to be on the client side.
@@ -64,7 +61,8 @@ void main(void)
         if (myPlayerType == Host)
         {
             // Initialize HOST-side threads
-            G8RTOS_AddThread( &menu_MenuHost, 0, 0xFFFFFFFF, "CREATE_GAME_____" ); // lowest priority
+            initCC3100(Host);
+            G8RTOS_AddThread( &menu_MenuHost, 15, 0xFFFFFFFF, "MENU_HOST" ); // lowest priority
             buttonUP_pressed = false;
             break;
         }
@@ -72,14 +70,15 @@ void main(void)
         else if ( myPlayerType == Client )
         {
             // Initialize CLIENT-side threads
-            G8RTOS_AddThread( &menu_MenuClient, 0, 0xFFFFFFFF, "JOIN_GAME_______" ); // lowest priority
+            initCC3100(Client);
+            G8RTOS_AddThread( &menu_MenuClient, 15, 0xFFFFFFFF, "MENU_CLIENT" ); // lowest priority
             buttonRIGHT_pressed = false;
             break;
         }
     }
 
     // Erase the main menu before beginning the game
-    menu_eraseMainMenu();
+    menu_eraseHostOrClient();
 
     // launch RTOS
     G8RTOS_Launch_Priority();
