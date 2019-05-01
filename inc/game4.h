cbca73bda7e5e5395c2f1a97393cf2f355095b40
Matthew Dolan <Matt@Matthews-MacBook-Air.local>
2019-05-01 19:52:22 -0400

    added end of game capability

--- inc/game4.h
+++ inc/game4.h
@@ -11,105 +11,101 @@
 #include "time.h"
 #include "math.h"
 #include "common_threads.h"
+#include "G8RTOS_Semaphores.h"
+#include "menu.h"
 /*********************************************** Includes ********************************************************************/
-*
- * Struct to be sent from the client to the host
- */
-typedef struct
-{
-    uint32_t IP_address;
-    int8_t displacementX;
-    int8_t displacementY;
-    uint8_t playerNumber;
-    uint8_t direction;
-    bool ready;
-    bool joined;
-    bool acknowledge;
-} Game2_SpecificPlayerInfo_t;
 
-/*
- * General player info to be used by both host and client
- * Client responsible for translation
- */
-typedef struct
-{
-    int16_t currentCenterX;
-    int16_t currentCenterY;
-    uint8_t direction;
-    bool drawRight;
-    uint16_t num_lives;
-    uint16_t RunTime;
-    bool kill;
-} Game2_GeneralPlayerInfo_t;
-/*
- * Struct of all the balls, only changed by the host
- */
-typedef struct
-{
-    int16_t currentCenterX;
-    int16_t currentCenterY;
-    int16_t xvel;
-    int16_t yvel;
-    bool alive;
-    bool kill;
-} Game2_Ball_t;
-/*
- * Struct of all the balls, only changed by the host
- */
-typedef struct
-{
-    int16_t currentCenterX;
-    int16_t currentCenterY;
-    int16_t xvel;
-    int16_t yvel;
-    bool alive;
-    bool kill;
-} Game2_Shroom_t;
+/*********************************************** Semaphores ******************************************************************/
+semaphore_t LCD_Semaphore;
 
-/*
- * Struct to be sent from the host to the client
- */
-typedef struct
-{
-    Game2_SpecificPlayerInfo_t player;
-    Game2_GeneralPlayerInfo_t players[MAX_NUM_OF_PLAYERS];
-    Game2_Ball_t balls[MAX_NUM_OF_BALLS];
-    Game2_Shroom_t shroom[MAX_NUM_OF_SHROOMS];
-    bool winner;
-    bool gameDone;
-} Game2_GameState_t;
-#pragma pack ( pop )
 
-/*
- * Struct of all the previous ball locations, only changed by self for drawing!
- */
-typedef struct
-{
-    int16_t CenterX;
-    int16_t CenterY;
-}Game2_PrevBall_t;
-
-/*
- * Struct of all the previous players locations, only changed by self for drawing
- */
-typedef struct
-{
-    int16_t CenterX;
-    int16_t CenterY;
-}Game2_PrevPlayer_t;
-
-/*
- * Struct of all the previous players locations, only changed by self for drawing
- */
-typedef struct
-{
-    int16_t CenterX;
-    int16_t CenterY;
-}Game2_PrevShroom_t;
 
 /*********************************************** Game Functions *********************************************************************/
 
 void game4_addHostThreads();    // called by the menu to initialize this game
+void game4_addClientThreads();  // called by the menu to initialize this game
+
+/*********************************************** Structures     *********************************************************************/
+
+// GAMESTATE data for both players
+typedef struct
+{
+
+    uint16_t BC_Color;
+    uint16_t Pipe_Color;
+
+    // bird locations
+    uint16_t Flappy_X;
+    uint16_t Flappy_Y;
+    int8_t  Flappy_Y_Velocity;
+
+    // pipe locations
+    uint16_t Pipe_Width;
+    uint8_t  Pipe_Gap;
+    int16_t Pipe_X;
+    uint16_t Pipe_Y;
+
+    uint8_t score;
+} game4_gamestate_t;
+
+
+
+// CLIENT TO HOST data structure
+typedef struct
+{
+    uint32_t IP_address;    // return data information
+    uint8_t playerNumber;   // return data information
+    bool joined;            // handshaking data
+    bool acknowledge;       // handshaking data
+    bool buttonPressed;     // push button data
+} game4_ClientToHost_t;
+
+/*
+ * HOST TO CLIENT data structure
+ * Game Number : 0 => top left
+ *               1 => top right
+ *               2 => bottom left
+ *               3 => bottom right
+*/
+typedef struct
+{
+    game4_ClientToHost_t client;
+    int8_t game_number;
+    int8_t prev_game_number;
+    bool choice_made;
+    game4_gamestate_t gamestate;
+    bool game4_endgame;
+} game4_HostToClient_t;
+
+
+
+/*********************************************** Client Threads *********************************************************************/
+/*
+ * Thread for client to join game
+ */
+void game4_JoinGame();
+
+/*
+ * Thread that receives game state packets from host
+ */
+void game4_ReceiveDataFromHost();
+
+/*
+ * Thread that sends UDP packets to host
+ */
+void game4_SendDataToHost();
+
+/*
+ * Thread to read client's joystick
+ */
+void game4_ReadJoystickClient();
+
+/*
+ * End of game for the client
+ */
+void game4_EndOfGameClient();
+
+/*********************************************** Client Threads *********************************************************************/
 
 
 /*********************************************** Host Threads *********************************************************************/
@@ -119,8 +115,24 @@ void game4_addHostThreads();    // called by the menu to initialize this game
 void game4_CreateGame();
 
 /*
+ * Thread for moving the bird on host side
+ */
+void game4_MoveBird();
+
+/*
+ * Thread that sets new location of pipes.
+ */
+void game4_MovePipes();
+
+/*
  * Thread that sends game state to client
  */
+void game4_SendDataToClient();
+
+/*
+ * Thread that receives UDP packets from client
+ */
+void game4_ReceiveDataFromClient();
 
 /*
  * Thread to read host's joystick
@@ -156,4 +168,4 @@ void game4_InitBoardState();
 /*********************************************** Aperiodic Threads *********************************************************************/
 void game4_ButtonPress ( void );
 
-#endif /* INC_GAME4_H_ */
+#endif /* INC_game4_H_ */
--- src/game4.c
+++ src/game4.c
@@ -19,136 +19,370 @@
 
 #include "game4.h"
 
-//add host threads
-void game4_addHostThreads(){
-    G8RTOS_AddThread( &Game2_GenerateBall, 20, 0xFFFFFFFF,          "GENERATE_BALL___" );
-    G8RTOS_AddThread( &game4_DrawObjects, 10, 0xFFFFFFFF,           "DRAW_OBJECTS____" );
-    G8RTOS_AddThread( &game4_ReadJoystickHost, 20, 0xFFFFFFFF,      "READ_JOYSTICK___" );
-    G8RTOS_AddThread( &game4_IdleThread, 255, 0xFFFFFFFF,           "IDLE____________" );
-    G8RTOS_AddThread(&Game2_MoveBall, 11, 0xFFFFFFFF, "MOVE_BALL_______");
+/*
+ ************************************************************** Sprite Maps ********************************************************************************
+ */
+
+
+
+
+/*
+ ******************************************************** LOCALS ************************************************************
+ */
+
+uint16_t matrix[4][4] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+uint16_t colorMatrix[8] = {LCD_BLUE,LCD_YELLOW,LCD_WHITE,LCD_PURPLE,LCD_RED,LCD_GREEN,LCD_ORANGE,LCD_PINK};
+
+uint16_t xcoord = 0;
+uint16_t ycoord = 0;
+
+uint16_t xOffset = 80;
+uint16_t yOffset = 60;
+
+
+
+/*
+ * Thread for client to join game
+ */
+void game4_JoinGame(){
+
 }
 
+/*
+ * Thread that receives game state packets from host
+ */
+void game4_ReceiveDataFromHost(){
+
+}
+
+/*
+ * Thread that sends UDP packets to host
+ */
+void game4_SendDataToHost(){
+
+}
+
+/*
+ * Thread to read client's joystick
+ */
+void game4_ReadJoystickClient(){
+
+}
+
+/*
+ * End of game for the client
+ */
+void game4_EndOfGameClient(){
+
+}
+
+/*********************************************** Client Threads *********************************************************************/
+
+
+/*********************************************** Host Threads *********************************************************************/
+/*
+ * Thread for the host to create a game
+ */
 void game4_CreateGame(){
-    game4_InitBoardState();
-    game4_addHostThreads();
+
+    G8RTOS_InitSemaphore(&LCD_Semaphore, 1);
+
+    common_buttons_init();
+    srand(0);
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 2; j++){
+            uint8_t row = rand()%4;
+            uint8_t col = rand()%4;
+            while(1){
+                if(matrix[row][col] == 0){
+                    matrix[row][col] = colorMatrix[i];
+                    break;
+                }
+                row = rand()%4;
+                col = rand()%4;
+            }
+        }
+    }
+    LCD_Clear(LCD_BLACK);
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            LCD_DrawRectangle(j*xOffset, (j+1)*xOffset-1, yOffset*i, (i+1)*yOffset-1, matrix[j][i]);
+
+        }
+    }
+
+    /*
+     * Add necessary host threads for reading the buttons, moving the bird, and drawing
+     */
+    G8RTOS_AddThread(&game4_DrawObjects, 10, 0xFFFFFFFF, "DrawObjects");
+    G8RTOS_AddThread(&game4_ReadJoystickHost, 15, 0xFFFFFFFF, "IdleThread");
+    G8RTOS_AddThread(&game4_IdleThread, 0xff, 0xFFFFFFFF, "IdleThread");
 
     G8RTOS_KillSelf();
 
 }
 
-void game4_InitBoardState(){
-    G8RTOS_WaitSemaphore(&LCDREADY);
-    //set inital lcd display
-    //define inital values
-    G8RTOS_SignalSemaphore(&LCDREADY);
+/*
+ * Thread that sends game state to client
+ */
+void game4_SendDataToClient(){
+
 }
+
+/*
+ * Thread that receives UDP packets from client
+ */
+void game4_ReceiveDataFromClient(){
+
+}
+
+/*
+ * Thread to read host's joystick
+ */
 void game4_ReadJoystickHost(){
-    int16_t avgX = 0;
-    int16_t avgY = 0;
-    int16_t joystick_x = 0;
-    int16_t joystick_y = 0;
+
+int16_t joystick_x = 0;
+
+int16_t joystick_y = 0;
+
+int16_t displacementX = 0;
+
+int16_t displacementY = 0;
+
+int16_t avgX = 0;
+
+int16_t avgY = 0;
+
 
     while(1)
+
     {
 
         GetJoystickCoordinates(&joystick_x, &joystick_y);
-        avgX = (avgX + joystick_x + JOYSTICK_BIAS_HOST) >> 1;
-        avgY = (avgY + joystick_y + JOYSTICK_BIAS_HOST) >> 1;
-        if(avgX < 750 && avgX > -1000){
-            avgX = 0;
-        }
-        if(avgY < 750 && avgY > -1000){
-            avgY = 0;
-        }
+
+
+
         // The switch statement was causing about 500 ms of lag
-        Game2_displacementX = -(avgX >> 10);
-        Game2_displacementY = (avgY >> 10);
 
-        gamestate.players[0].currentCenterX += Game2_displacementX;
-        gamestate.players[0].currentCenterY += Game2_displacementY;
+        if((joystick_x < -1000 || joystick_x > 1000) && abs(joystick_x) > abs(joystick_y))
+            displacementX += (joystick_x >> 2);
 
-        gamestate.players[1].currentCenterX += gamestate.player.displacementX;
-        gamestate.players[1].currentCenterY += gamestate.player.displacementY;
+        else if(joystick_y < -1000 || joystick_y > 1000)
+        displacementY += -(joystick_y >> 2);
 
-        // PLAYER 1's X LIMITATIONS---------------------------------------------
-        // player center is too far to the left - limit it.
-        if ( gamestate.players[0].currentCenterX - PLAYER_OFFSET - 1 <= ARENA_MIN_X )
-        {
-            gamestate.players[0].currentCenterX = ARENA_MIN_X + PLAYER_OFFSET + 1;
+        uint16_t scale = 15000;
+
+        if(displacementX < -2 * scale){
+            displacementX = -2 * scale;
+        }
+        else if(displacementX > 2 * scale){
+            displacementX = 2 * scale;
         }
 
-        // player center is too far to the right - limit it.
-        else if ( gamestate.players[0].currentCenterX + PLAYER_OFFSET + 1 > ARENA_MAX_X - MARIO_COLUMNS )
-        {
-            gamestate.players[0].currentCenterX = ARENA_MAX_X - PLAYER_OFFSET - 1 - MARIO_COLUMNS;
+        if(displacementY < -2 * scale){
+            displacementY = -2 * scale;
         }
-
-        // PLAYER 1's Y LIMITATIONS---------------------------------------------
-        // player center is too far to the left - limit it.
-        if ( gamestate.players[0].currentCenterY - PLAYER_OFFSET - 1 <= ARENA_MIN_Y )
-        {
-            gamestate.players[0].currentCenterY = ARENA_MIN_Y + PLAYER_OFFSET + 1;
-        }
-
-        // player center is too far to the right - limit it.
-        else if ( gamestate.players[0].currentCenterY + PLAYER_OFFSET + 1 > ARENA_MAX_Y - 1 - MARIO_ROWS )
-        {
-            gamestate.players[0].currentCenterY = ARENA_MAX_Y - PLAYER_OFFSET - 1 - MARIO_ROWS;
+        else if(displacementY > 2 * scale){
+            displacementY = 2 * scale;
         }
 
 
+        // determine what square to hi-lite based off the joystick displacement
 
-        // PLAYER 2's X LIMITATIONS---------------------------------------------
-        // player center is too far to the left - limit it.
-        if ( gamestate.players[1].currentCenterX - PLAYER_OFFSET - 1 <= ARENA_MIN_X )
-        {
-            gamestate.players[1].currentCenterX = ARENA_MIN_X + PLAYER_OFFSET + 1;
+        // X at right
+        if(displacementX < -1*scale){
+            xcoord = 3;
         }
 
-        // player center is too far to the right - limit it.
-        else if ( gamestate.players[1].currentCenterX + PLAYER_OFFSET + 1 > ARENA_MAX_X - MARIO_COLUMNS )
-        {
-            gamestate.players[1].currentCenterX = ARENA_MAX_X - PLAYER_OFFSET - 1 - MARIO_COLUMNS;
+        // X at right Middle
+        else if(displacementX < 0){
+            xcoord = 2;
         }
 
-        // PLAYER 1's Y LIMITATIONS---------------------------------------------
-        // player center is too far to the left - limit it.
-        if ( gamestate.players[1].currentCenterY - PLAYER_OFFSET - 1 <= ARENA_MIN_Y )
-        {
-            gamestate.players[1].currentCenterY = ARENA_MIN_Y + PLAYER_OFFSET + 1;
+        // X at left Middle
+        else if(displacementX < scale){
+            xcoord = 1;
         }
 
-        // player center is too far to the right - limit it.
-        else if ( gamestate.players[1].currentCenterY + PLAYER_OFFSET + 1 > ARENA_MAX_Y - 1 - MARIO_ROWS )
-        {
-            gamestate.players[1].currentCenterY = ARENA_MAX_Y - PLAYER_OFFSET - 1 - MARIO_ROWS;
+        // X at left
+        else{
+            xcoord = 0;
         }
 
 
 
-        // player direction handling for the host
-        if ( abs(Game2_displacementX) >= abs(Game2_displacementY) )
-        {
-            if (Game2_displacementX > 0 )
-                gamestate.players[0].direction = 3;
-
-            else
-                gamestate.players[0].direction = 1;
-        }
-        else
-        {
-
-            if (Game2_displacementY > 0 )
-                gamestate.players[0].direction = 0;
-            else
-                gamestate.players[0].direction = 2;
+        // Y at top
+        if(displacementY < -1*scale){
+            ycoord = 3;
         }
 
-        sleep(10);
+        // Y at Top Middle
+        else if(displacementY < 0){
+            ycoord = 2;
+        }
+
+        // Y at Bottom Middle
+        else if(displacementY < scale){
+            ycoord = 1;
+        }
+
+        // Y at bottom
+        else{
+            ycoord = 0;
+        }
+
+
+        sleep(20);
+
     }
+
 }
+
+/*
+ * End of game for the host
+ */
+void game4_EndOfGameHost(){
+    G8RTOS_WaitSemaphore(&LCD_Semaphore);
+    G8RTOS_KillAllOthers();
+    G8RTOS_InitSemaphore(&LCD_Semaphore, 1);
+
+    LCD_Clear(LCD_BLACK);
+    delay(1000000);
+
+    G8RTOS_AddThread(&menu_MenuHost, 10, 0xFFFFFFFF, "MenuHost");
+
+    G8RTOS_KillSelf();
+}
+
+/*********************************************** Host Threads *********************************************************************/
+
+
+/*********************************************** Common Threads *********************************************************************/
+/*
+ * Idle thread
+ */
 void game4_IdleThread(){
+    while(1);
+
+}
+
+/*
+ * Thread to draw all the objects in the game
+ */
+void game4_DrawObjects(){
+
+    sleep(1000);
+    LCD_Clear(LCD_BLACK);
+    uint16_t xcoord_Old = 5;
+    uint16_t ycoord_Old = 5;
+
+    uint8_t keepHiLited = 0;
+
+    uint16_t first_xcoord = 5;
+    uint16_t first_ycoord = 5;
+    uint16_t second_xcoord = 5;
+    uint16_t second_ycoord = 5;
+
+    uint8_t valid[4][4] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+    uint8_t turn = 0;
+    uint8_t decision = 0;
+
+    uint8_t valid_cnt = 0;
+
+
     while(1){
 
+        if(buttonUP_pressed){
+            sleep(50);
+            buttonUP_pressed = 0;
+            if(!valid[xcoord][ycoord]){
+                LCD_DrawRectangle(xcoord*xOffset + 8, xcoord*xOffset + xOffset - 9, ycoord*yOffset + 8, ycoord*yOffset + yOffset - 9, matrix[xcoord][ycoord]);
+
+                if(turn){
+                    turn = 0;
+                    second_xcoord = xcoord;
+                    second_ycoord = ycoord;
+                    valid[xcoord][ycoord] = 1;
+                    decision = 1;
+                    valid_cnt++; //keeps track of progress towards end of game
+                }
+                else{
+                    first_xcoord = xcoord;
+                    first_ycoord = ycoord;
+                    valid[xcoord][ycoord] = 1;
+                    valid_cnt++;//keeps track of progress towards end of game
+                    turn = 1;
+                    keepHiLited = 1;
+                }
+            }
+        }
+
+        else if(xcoord != xcoord_Old || ycoord != ycoord_Old){
+            if(keepHiLited || valid[xcoord_Old][ycoord_Old]){
+                LCD_DrawRectangle(xcoord_Old*xOffset, xcoord_Old*xOffset + xOffset - 1, ycoord_Old*yOffset, ycoord_Old*yOffset + yOffset - 1, LCD_BLACK);
+                LCD_DrawRectangle(xcoord_Old*xOffset + 2, xcoord_Old*xOffset + xOffset - 3, ycoord_Old*yOffset + 2, ycoord_Old*yOffset + yOffset - 3, matrix[xcoord_Old][ycoord_Old]);
+                keepHiLited = 0;
+            }
+            else{
+                LCD_DrawRectangle(xcoord_Old*xOffset, xcoord_Old*xOffset + xOffset - 1, ycoord_Old*yOffset, ycoord_Old*yOffset + yOffset - 1, LCD_BLACK);
+            }
+
+            LCD_DrawRectangle(xcoord*xOffset, xcoord*xOffset + xOffset - 1, ycoord*yOffset, ycoord*yOffset + yOffset - 1, LCD_WHITE);
+            LCD_DrawRectangle(xcoord*xOffset + 4, xcoord*xOffset + xOffset - 5, ycoord*yOffset + 4, ycoord*yOffset + yOffset - 5, LCD_BLACK);
+            if(valid[xcoord][ycoord] == 1){
+                LCD_DrawRectangle(xcoord*xOffset + 8, xcoord*xOffset + xOffset - 9, ycoord*yOffset + 8, ycoord*yOffset + yOffset - 9, matrix[xcoord][ycoord]);
+            }
+            xcoord_Old = xcoord;
+            ycoord_Old = ycoord;
+        }
+
+        if(decision){
+            sleep(300);
+
+            if(matrix[first_xcoord][first_ycoord] != matrix[second_xcoord][second_ycoord]){
+                valid[first_xcoord][first_ycoord] = 0;
+                valid[second_xcoord][second_ycoord] = 0;
+
+                if(first_xcoord == xcoord && first_ycoord == ycoord)
+                    LCD_DrawRectangle(xcoord*xOffset + 4, xcoord*xOffset + xOffset - 5, ycoord*yOffset + 4, ycoord*yOffset + yOffset - 5, LCD_BLACK);
+                else
+                    LCD_DrawRectangle(first_xcoord*xOffset, first_xcoord*xOffset + xOffset - 1, first_ycoord*yOffset, first_ycoord*yOffset + yOffset - 1, LCD_BLACK);
+
+                if(second_xcoord == xcoord && second_ycoord == ycoord)
+                    LCD_DrawRectangle(xcoord*xOffset + 4, xcoord*xOffset + xOffset - 5, ycoord*yOffset + 4, ycoord*yOffset + yOffset - 5, LCD_BLACK);
+                else
+                    LCD_DrawRectangle(second_xcoord*xOffset, second_xcoord*xOffset + xOffset - 1, second_ycoord*yOffset, second_ycoord*yOffset + yOffset - 1, LCD_BLACK);
+
+            }
+            decision = 0;
+        }
+       if(valid_cnt == 16){
+           G8RTOS_AddThread(game4_EndOfGameHost, 0, 0xFFFFFFFF, "END_OF_GAME_HOST");
+       }
+
+
+
+
+
+
+
+
+
+        sleep(30);
     }
+
+}
+
+/*********************************************** Common Functions *********************************************************************/
+
+/*
+ * Initializes and prints initial game state
+ */
+void game4_InitBoardState(){
+
+}
+
+/*********************************************** Aperiodic Threads *********************************************************************/
+void game4_ButtonPress ( void ){
 }
